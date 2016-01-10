06c695097027876059285dedc646e7434cff6e50
Levi Botelho <levi.botelho@gmail.com>
2016-01-10 18:17:08 +0100

    Add testing to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 var gulp = require("gulp");
 var eslint = require("gulp-eslint");
+var jasmine = require('gulp-jasmine');
 
-gulp.task("default", function () {
+gulp.task("lint", function () {
 	return gulp.src("./index.js")
 		.pipe(eslint())
 		.pipe(eslint.formatEach())
 		.pipe(eslint.failAfterError());
+});
+
+gulp.task("default", ["lint"], function () {
+	gulp.src("./spec/global-spec.js")
+		.pipe(jasmine());
 });
