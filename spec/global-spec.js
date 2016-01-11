1f8e01b3ee7257255d78b1570597a7603878d1bb
Levi Botelho <levi.botelho@gmail.com>
2016-01-11 21:30:22 +0100

    Add more GET tests

--- spec/global-spec.js
+++ spec/global-spec.js
@@ -19,11 +19,14 @@ function getStubbedAjax(stub) {
 }
 
 describe("The get method", function () {
-	var ajax, stub, wasOpenCalled, wasSendCalled, headers = {};
+	var wasOpenCalled, wasSendCalled, headers = {}, calledVerb, targetUrl, openedAsync;
 	beforeAll(function (done) {
-		stub = {
-			open: function () {
+		var stub = {
+			open: function (verb, url, async) {
 				wasOpenCalled = true;
+				calledVerb = verb;
+				targetUrl = url;
+				openedAsync = async;
 			},
 			send: function () {
 				wasSendCalled = true;
@@ -35,7 +38,7 @@ describe("The get method", function () {
 				headers[key] = value;
 			}
 		};
-		ajax = getStubbedAjax(stub);
+		var ajax = getStubbedAjax(stub);
 		ajax.get(exampleUrl)
 			.then(done, fail);
 	});
@@ -56,6 +59,83 @@ describe("The get method", function () {
 
 	it("does not set the Content-Type header", function () {
 		expect(headers["Content-Type"]).toBe(undefined);
+	});
+
+	it("opens the connection with the GET verb", function () {
+		expect(calledVerb).toBe("GET");
+	});
+
+	it("opens the connection to the correct URL", function () {
+		expect(targetUrl).toBe(exampleUrl);
+	});
+
+	it("opens the connection as async", function () {
+		expect(openedAsync).toBe(true);
+	});
+
+	describe("calls the onBeforeSend callback", function () {
+		var onBeforeSendCalled, passedRequest, passedVerb, passedUrl;
+		beforeAll(function (done) {
+			var stub = {
+				open: function () {},
+				send: function () {},
+				setRequestHeader: function () {}
+			};
+			var ajax = getStubbedAjax(stub);
+			ajax.get(exampleUrl, null, {
+				onBeforeSend: function (request, verb, url) {
+					onSendCalled = true;
+					passedRequest = request;
+					passedVerb = verb;
+					passedUrl = url;
+				}
+			}).then(done, fail);
+		});
+
+		it("when one is provided", function () {
+			expect(onSendCalled).toBe(true);
+		});
+
+		it("with a request object as the first parameter", function () {
+			// Good enough test that it's the XHR object.
+			expect(typeof passedRequest.onreadystatechange).toBe("function");
+		});
+
+		it("with the GET verb as the second parameter", function () {
+			expect(passedVerb).toBe("GET");
+		});
+
+		it("with the URL as the third parameter", function () {
+			expect(passedUrl).toBe(exampleUrl);
+		});
+	});
+
+	describe("when passed parameters", function () {
+		var baseUrl = exampleUrl;
+		var openedUrl, passedPayload;
+		beforeAll(function (done) {
+			var stub = {
+				open: function (verb, url) {
+					console.log("open results:" + verb + " " + url);
+					openedUrl = url;
+				},
+				send: function (payload) {
+					passedPayload = payload;
+				},
+				setRequestHeader: function () {}
+			};
+			var ajax = getStubbedAjax(stub);
+			ajax.get(baseUrl, {foo: "bar baz"})
+				.then(done, fail);
+		});
+
+		it("encodes them into the URL query string", function () {
+			expect(openedUrl).toBe(baseUrl + "?bar%20baz");
+		});
+
+		it("does not pass them as a payload", function () {
+			expect(typeof payload).toBe("undefined");
+		});
 	});
 });
 
